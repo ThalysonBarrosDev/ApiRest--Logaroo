dbbd15c97774e09867aa601da43766d3eeb55aad
Thalyson Barros <thalysonbarrospereira2019@gmail.com>
2022-10-30 12:10:22 -0300

    CHORE: Implementando autenticação JWT

---
+++ app/Http/Controllers/v1/AuthController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\v1;
+
+use Exception;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Providers\Auth\LoginService;
+
+class AuthController extends Controller {
+
+    private $loginService;
+
+    public function __construct(LoginService $loginService) {
+        
+        $this->loginService = $loginService;
+
+    }
+    
+    public function login(Request $request) {
+    
+        try {
+
+            $credentials = $request->only('email', 'password');
+            $auth = $this->loginService->execute($credentials);
+
+            return response()->json($auth, 200);
+
+        } catch (Exception $e) {
+
+            return response()->json(['error' => true, 'message' => 'Erro ao realizar a autenticação, verifique!'], $e->getCode()); 
+
+        }
+        
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\v1\AuthController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::prefix('v1/')->group(function () {
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::get('status', function () {
+        return response()->json(['api_name' => 'apirest-logaroo', 'status' => true], 200);
+    });
+
+    /* Route Authentication */
+    Route::post('auth/login', [AuthController::class, 'login']);
+
 });
