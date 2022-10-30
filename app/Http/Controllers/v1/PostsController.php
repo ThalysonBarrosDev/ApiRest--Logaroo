b584800936cf85a881376b6b80e18a15d2021fee
Thalyson Barros <thalysonbarrospereira2019@gmail.com>
2022-10-30 12:29:50 -0300

    CHORE: Implementando Routes e Middleware de autenticação

---
+++ app/Http/Controllers/v1/PostsController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers\v1;
+
+use Exception;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+
+class PostsController extends Controller
+{
+
+    public function create()
+    {
+
+        try {
+
+            return response()->json(['success' => true], 200);
+
+        } catch (Exception $e) {
+
+            return response()->json(['return' => $e->getMessage()], $e->getCode());
+
+        }
+    }
+
+    public function readAll()
+    {
+
+        try {
+
+            return response()->json(['success' => true], 200);
+
+        } catch (Exception $e) {
+
+            return response()->json(['return' => $e->getMessage()], $e->getCode());
+
+        }
+    }
+
+    public function read($tag)
+    {
+
+        try {
+
+            return response()->json(['success' => true], 200);
+
+        } catch (Exception $e) {
+
+            return response()->json(['return' => $e->getMessage()], $e->getCode());
+
+        }
+    }
+
+    public function update($id)
+    {
+
+        try {
+
+            return response()->json(['success' => true], 200);
+
+        } catch (Exception $e) {
+
+            return response()->json(['return' => $e->getMessage()], $e->getCode());
+
+        }
+    }
+
+    public function delete($id)
+    {
+
+        try {
+
+            return response()->json(['success' => true], 200);
+
+        } catch (Exception $e) {
+
+            return response()->json(['return' => $e->getMessage()], $e->getCode());
+
+        }
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -3,14 +3,28 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\v1\AuthController;
+use App\Http\Controllers\v1\PostsController;
+use App\Http\Middleware\v1\ProtectedRouteAuth;
 
 Route::prefix('v1/')->group(function () {
 
+    /* Route Status */
     Route::get('status', function () {
         return response()->json(['api_name' => 'apirest-logaroo', 'status' => true], 200);
     });
 
     /* Route Authentication */
     Route::post('auth/login', [AuthController::class, 'login']);
+
+    Route::middleware([ProtectedRouteAuth::class])->group(function() {
+
+        /* Route Posts */
+        Route::post('posts', [PostsController::class, 'create']);
+        Route::get('posts', [PostsController::class, 'readAll']);
+        Route::get('posts/{id}', [PostsController::class, 'read']);
+        Route::put('posts/{id}', [PostsController::class, 'update']);
+        Route::delete('posts/{id}', [PostsController::class, 'delete']);
+
+    });
 
 });
